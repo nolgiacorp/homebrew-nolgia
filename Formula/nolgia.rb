9664a0f65b3744927661a5d5400afd893945a740
Nick L. <lesseosnick@gmail.com>
2026-07-05 17:58:37 -0700

    nolgia 0.2.5

--- Formula/nolgia.rb
+++ Formula/nolgia.rb
@@ -1,17 +1,17 @@
 class Nolgia < Formula
   desc "CLI for the Nolgia generative media platform (image, audio, video)"
   homepage "https://nolgia.ai"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
 
   on_macos do
     url "https://github.com/nolgiacorp/nolgia-cli/releases/download/v#{version}/nolgia-x86_64-apple-darwin"
-    sha256 "00b0d4afb7e477965f583d9f0bdc9485a6c35d2a61df260fb8a3e64f981fe7c8"
+    sha256 "1b5cca9522712bb5b4b0c76b50d37ff0268dcf01bf6874389120e2fe356c31d6"
   end
 
   on_linux do
     url "https://github.com/nolgiacorp/nolgia-cli/releases/download/v#{version}/nolgia-x86_64-unknown-linux-gnu"
-    sha256 "9d83a522550b0962b692aed219617917ca38440ddc5b1f0efb03d66ceb28c0b1"
+    sha256 "3d9cb72495f57e97281b8ebebf2b8b71eb9aeeb7f98c057d5e5a70b1da84a4fe"
   end
 
   def install
